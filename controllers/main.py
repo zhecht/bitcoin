44e57b2630f6b9fabb3162b0036e1840c795218e
Zack Hecht <hechtor@Zacks-MacBook-Pro.local>
2017-12-20 22:43:21 -0500

    bitcoin

---
+++ controllers/main.py
@@ -0,0 +1,82 @@
+from flask import *
+import json
+from bs4 import BeautifulSoup as BS
+try:
+  import urllib2 as urllib
+except:
+  import urllib.request as urllib
+
+main = Blueprint('main', __name__, template_folder='views')
+
+owned = [
+  "btc", 0.03270468,
+  "eth", 0.24861740,
+  "ltc", 0.56849907,
+  "req", 197.802,
+  "wabi", 14.985,
+  "neo", .45,
+  #"icx", 5,
+  "vet", 18.966,
+  "qtum", .50872,
+  "eng", 23.976,
+  "trx", 430.569
+]
+
+owned_dict = {
+  "btc": 0.03270468,
+  "eth": 0.24861740,
+  "ltc": 0.56849907,
+  "req": 197.802,
+  "wabi": 14.985,
+  "neo": .45,
+  #"icx": 5,
+  "vet": 18.966,
+  "qtum": .50872,
+  "eng": 23.976,
+  "trx": 430.569
+}
+
+full_names = {
+  "btc": "bitcoin",
+  "eth": "ethereum",
+  "ltc": "litecoin",
+  "req": "request-network",
+  "wabi": "wabi",
+  "neo": "neo",
+  #"icx": "icon",
+  "vet": "vechain",
+  "qtum": "qtum",
+  "eng": "enigma-project",
+  "trx": "tron"
+}
+
+
+@main.route('/')
+def main_route():
+  coin_row = []
+  for coin in owned_dict:
+    #print(i, owned[i])
+    coin_row.append({"id": coin, "amt": owned_dict[coin]})
+  return render_template("main.html",owned=owned_dict,coin_rows=coin_row)
+
+@main.route('/price')
+def price_route():
+  arr = {}
+  url = "https://coinmarketcap.com/all/views/all/"
+  soup = BS(urllib.urlopen(url).read(), "lxml")
+
+  for coin in owned_dict:
+    full_id = "id-"+full_names[coin]
+    row = soup.find("tr", {"id": full_id})
+    #row = coin.find_all("tr", id=full_id)
+    val = row.find("a", class_="price")
+    inc_dec = row.find("td", class_="percent-24h").text
+    inc_dec_1h = row.find("td", class_="percent-1h").text
+    arr[coin] = {
+      "price": float(val.text[1:]),
+      "24h": inc_dec,
+      "1h": inc_dec_1h,
+    }
+
+  #print(arr)
+  return jsonify(arr)
---
+++ static/js/main.js
@@ -0,0 +1,92 @@
+
+coins = [
+  "btc",
+  "eth",
+  "ltc",
+  "req",
+  "wabi",
+  "neo",
+  //"icx",
+  "vet",
+  "qtum",
+  "eng",
+  "trx"
+]
+
+owned = {
+  "btc": 0.03270468,
+  "eth": 0.24861740,
+  "ltc": 0.56849907,
+  "req": 197.802,
+  "wabi": 14.985,
+  "neo": .45,
+  //"icx": 5,
+  "vet": 18.966,
+  "qtum": 0.50872,
+  "eng": 23.976,
+  "trx": 430.569
+}
+
+bought = {
+  "btc": 18345.99,
+  "eth": 804.45,
+  "ltc": 351.8,
+  "req": .278,
+  "wabi": 1.6,
+  "neo": 69,
+  //"icx": 2.05,
+  "vet": 1.46,
+  "qtum": 63,
+  "eng": 1.19,
+  "trx": 0.048807
+}
+
+function timeout() {
+  $.get("/price", function(data) {
+    var overall_profit = 0;
+
+    for (var i = 0; i < coins.length; ++i) {
+      var price = data[coins[i]]["price"];
+      var day_trend = data[coins[i]]["24h"];
+      var hour_trend = data[coins[i]]["1h"];
+      var curr_total = (price * owned[coins[i]]).toFixed(2);
+      var bought_total = (bought[coins[i]] * owned[coins[i]]).toFixed(2);
+      var total_profit = (curr_total - bought_total).toFixed(2);
+      
+      overall_profit += parseFloat(total_profit);
+      $("#"+coins[i]+" .price").text("$"+price);
+      $("#"+coins[i]+" .24h").text(day_trend);
+      $("#"+coins[i]+" .1h").text(hour_trend);
+
+      $("#"+coins[i]+" .profit").text("$"+curr_total);
+      $("#"+coins[i]+" .bought").text("$"+bought_total);
+      $("#"+coins[i]+" .total_profit").text("$"+total_profit);
+
+      if (day_trend[0] === "-") {
+        $("#"+coins[i]+" .24h").css("color", "red");
+      } else {
+        $("#"+coins[i]+" .24h").css("color", "green");
+      }
+      if (hour_trend[0] === "-") {
+        $("#"+coins[i]+" .1h").css("color", "red");
+      } else {
+        $("#"+coins[i]+" .1h").css("color", "green");
+      }
+      if (total_profit < 0) {
+        $("#"+coins[i]+" .total_profit").css("color", "red");
+      } else {
+        $("#"+coins[i]+" .total_profit").css("color", "green");
+      }
+    }
+
+    $("#my_profit").text("$"+overall_profit.toFixed(2));
+    if (overall_profit < 0) {
+      $("#my_profit").css("color", "red");
+    } else {
+      $("#my_profit").css("color", "green");
+    }
+  });
+}
+
+timeout();
+setInterval(timeout, 10000);
