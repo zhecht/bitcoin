44de7934092f3f03340fe737a40b1ad5284aeb36
Zack Hecht <hechtor@Zacks-MBP.fios-router.home>
2017-12-24 17:29:55 -0500

    nick

--- controllers/main.py
+++ controllers/main.py
@@ -134,7 +134,7 @@ def price_route(name):
     full_id = "id-"+full_names[coin]
     row = soup.find("tr", {"id": full_id})
     val = row.find("a", class_="price")
-    inc_dec = row.find("td", class_="percent-24h").text
+    inc_dec = row.find_all("td")[-3].text
     inc_dec_1h = row.find_all("td")[-2].text
     inc_dec_7d = row.find_all("td")[-1].text
     
